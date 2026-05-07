74902a57d536d1cc0c1dd657952a6bf2b6a263d2
354 <agame6461@gmail.com>
2026-05-07 09:55:37 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'persinas-v2';
+const ASSETS = [
+  './persinas_asad_absensi.html',
+  './persinas-asad-.png',
+  './manifest.json',
+  './index.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.url.includes('supabase.co')) return;
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
